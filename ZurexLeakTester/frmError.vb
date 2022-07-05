c20ccb3191f20a2dd277d0473cf60178a8ba92a9
wyatt-cuypers <102075671+wyatt-cuypers@users.noreply.github.com>
2022-07-05 16:10:58 -0500

    Update frmError.vb

--- ZurexLeakTester/frmError.vb
+++ ZurexLeakTester/frmError.vb
@@ -2,12 +2,13 @@
 
     Private WithEvents tmrErrorPage As New Timer
     Private Sub frmError_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'Timer interval is set to five seconds
         tmrErrorPage.Interval = 5000
         tmrErrorPage.Start()
     End Sub
 
     Private Sub tmrErrorPage_Tick(ByVal sender As Object, ByVal e As System.EventArgs) Handles tmrErrorPage.Tick
-        'Closes the form after three seconds
+        'Closes the form after five seconds
         Application.Exit()
         Close()
     End Sub
